f0ad942f894bffb85bd28b9c881cc141591a7080
Pedro Ringuelet <peter.ringuelet@gmail.com>
2025-09-01 20:22:43 -0300

    mapa casi listo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import InteractiveSVG from '@/components/InteractiveSVG';
 
 export default function Page() {
   return (
-    <main className="flex items-center justify-center bg-gray-100 min-h-screen h-screen w-screen overflow-hidden">
+    <main className="flex items-center justify-center bg-gray-100 min-h-[100dvh] h-[100dvh] w-screen overflow-auto sm:overflow-hidden">
       <InteractiveSVG className="w-full max-w-screen h-auto" />
     </main>
   )
